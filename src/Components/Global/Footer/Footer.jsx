247491b92b97c728f1751f9abeea78394402070d
Md. Hazrat Ali <md.hazratali4510@gmail.com>
2023-05-18 22:19:42 +0600

    Should use this folder structure

---
+++ src/Components/Global/Footer/Footer.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Footer.scss";
+
+const Footer = () => {
+  return <footer id="footer">{/* Footer Code Here */}</footer>;
+};
+
+export default Footer;
---
+++ src/Components/Global/Navbar/Navbar.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Navbar.scss";
+
+const Navbar = () => {
+  return <nav id="navbar">{/* Navbar Code here */}</nav>;
+};
+
+export default Navbar;
